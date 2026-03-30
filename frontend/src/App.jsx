db7732a118a1c09698329bdb756afd8280c065ea
nghiagenz <nghiagenz2@gmail.com>
2026-03-30 10:17:59 +0700

    update route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import "./styles/firestaff-shell.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Splash from './components/Splash.jsx';
 import Onboarding from './components/Onboarding.jsx';
+import LoginForm from './components/LoginForm.jsx';
 import ScrollToTop from "./components/ScrollToTop.jsx";
 import ResidentGuidancePage from "./pages/resident/ResidentGuidancePage.jsx";
 import ResidentDevicePage from "./pages/resident/ResidentDevicePage.jsx";
@@ -23,6 +24,7 @@ function App() {
         {/* Auth Flow */}
         <Route path="/splash" element={<Splash />} />
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route
           path="/firestaff/home"
           element={<Navigate to="/firestaff/simulation" replace />}
@@ -39,14 +41,8 @@ function App() {
         />
 
         {/* Resident Pages */}
-        <Route
-          path="/resident/home"
-          element={<Navigate to="/resident/devices" replace />}
-        />
-        <Route
-          path="/resident/escape"
-          element={<Navigate to="/resident/guidance" replace />}
-        />
+        <Route path="/resident/home" element={<ResidentHomePage />} />
+        <Route path="/resident/escape" element={<ResidentEscapePage />} />
         <Route path="/resident/devices" element={<ResidentDevicePage />} />
         <Route path="/resident/guidance" element={<ResidentGuidancePage />} />
 
